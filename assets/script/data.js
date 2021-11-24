b3828d5cbe80c3bd660f714343f74d8918e6b130
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-23 21:15:03 -0300

    Create data.js

---
+++ assets/script/data.js
@@ -0,0 +1,20 @@
+window.audios = [
+  {
+    title: "stars",
+    artist: "lofi type beat",
+    cover: "stars.jpg",
+    file: "stars.mp3"
+  },
+  {
+    title: "sunrise",
+    artist: "lofi type beat",
+    cover: "sunrise.jpg",
+    file: "sunrise.mp3"
+},
+ {
+    title: "sunset",
+    artist: "Kina (ft. Shiloh)",
+    cover: "sunset.jpg",
+    file: "sunset.mp3"
+  }
+];
