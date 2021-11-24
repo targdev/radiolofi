7ca8011bfc71a469b66d55ed6b204bf4c9456a69
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-23 21:15:01 -0300

    Create script.js

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+// alert('relaxe... sinta a vibe')
+
+const body = document.querySelector('body');
+const cover = document.querySelector('.card-image');
+const title = document.querySelector('.card-content h5');
+const artist = document.querySelector('.artist');
+const audio = document.querySelector('audio');
+
+const dataBase = {
+    title: "s t a r s",
+    artist: "lofi type beat",
+    cover: "assets/files/stars.jpg",
+    file: "s t a r s.mp3"
+  }
+
+cover.style.background = white
