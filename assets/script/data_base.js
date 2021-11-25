22f86a4dbe57b599a33d5de2d075ca557d163274
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-25 15:31:09 -0300

    Create data_base.js

---
+++ assets/script/data_base.js
@@ -0,0 +1,20 @@
+window.audios = [
+  {
+    title: "stars",
+    artist: "lofi type beat",
+    cover: "assets/files/stars.jpg",
+    file: "assets/files/stars.mp3"
+  },
+  {
+    title: "sunrise",
+    artist: "lofi type beat",
+    cover: "assets/files/sunrise.jpg",
+    file: "assets/files/sunrise.mp3"
+},
+ {
+    title: "sunset",
+    artist: "Kina (ft. Shiloh)",
+    cover: "assets/files/sunset.jpg",
+    file: "assets/files/sunset.mp3"
+  }
+];
