272d54a1ac209bafd95399d90c17c20dadd58031
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-25 17:44:56 -0300

    Update player.js

--- assets/script/player.js
+++ assets/script/player.js
@@ -1,5 +1,5 @@
-
 window.player = {
+    main: document.querySelector('#main'),
     title: document.querySelector('.card-content h5'),
     artist: document.querySelector('.artist'),
     cover: document.querySelector('.card-image'),
@@ -10,19 +10,31 @@ window.player = {
     currentPlaying: 0,
   
     start() {
+      this.update();
+      this.audio.onended = () => this.next();
+    },
 
+    next() {
+      this.currentPlaying++
+      this.restart();
+      this.update();
+      this.audio.play();
+    },
+
+    update() {
       this.currentAudio = this.audioData[this.currentPlaying];
 
       this.title.innerText = this.currentAudio.title;
       this.artist.innerText = this.currentAudio.artist;
       this.cover.style.background = `url(${path(this.currentAudio.cover)}) no-repeat center center / cover`;
       this.audio.src = path(this.currentAudio.file);
+    },
 
-      this.audio.addEventListener("ended", () => {
-        this.currentPlaying++
-        this.audio.src = path(this.audioData[this.currentPlaying].file);
-        this.audio.play();
-      })
+    restart() {
+      if(this.currentPlaying == this.audioData.length){
+        this.currentPlaying = 0;
+        this.update();
+      }
     }
 
   }
