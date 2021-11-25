50eeb63a85aa8ef2546d998cf17eaea9f27b6f08
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-25 17:04:28 -0300

    Update data_base.js

--- assets/script/data_base.js
+++ assets/script/data_base.js
@@ -2,19 +2,19 @@ window.audios = [
   {
     title: "stars",
     artist: "lofi type beat",
-    cover: "assets/files/stars.jpg",
-    file: "assets/files/stars.mp3"
+    cover: "stars.jpg",
+    file: "stars.mp3"
   },
   {
     title: "sunrise",
     artist: "lofi type beat",
-    cover: "assets/files/sunrise.jpg",
-    file: "assets/files/sunrise.mp3"
+    cover: "sunrise.jpg",
+    file: "sunrise.mp3"
 },
  {
     title: "sunset",
     artist: "Kina (ft. Shiloh)",
-    cover: "assets/files/sunset.jpg",
-    file: "assets/files/sunset.mp3"
+    cover: "sunset.jpg",
+    file: "sunset.mp3"
   }
 ];
