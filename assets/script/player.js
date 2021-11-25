3470a94913f13f7e4e3635c46bfc9c6bd914c95f
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-25 16:47:32 -0300

    Update player.js

--- assets/script/player.js
+++ assets/script/player.js
@@ -4,18 +4,17 @@ window.player = {
     title: document.querySelector('.card-content h5'),
     artist: document.querySelector('.artist'),
     audio: document.querySelector('audio'),
-  
-    currentAudio: {
-      title: "s t a r s",
-      artist: "lofi type beat",
-      cover: "assets/files/stars.jpg",
-      file: "assets/files/stars.mp3"
-    },
+
+    audioData: audios,
+    currentAudio: {},
   
     start() {
-      this.cover.style.background = `url(${this.dataBase.cover}) no-repeat center center / cover`;
-      this.title.innerText = this.dataBase.title;
-      this.artist.innerText = this.dataBase.artist;
-      this.audio.src = this.dataBase.file;
+
+      this.currentAudio = this.audioData[0]
+
+      this.cover.style.background = `url(${this.currentAudio.cover}) no-repeat center center / cover`;
+      this.title.innerText = this.currentAudio.title;
+      this.artist.innerText = this.currentAudio.artist;
+      this.audio.src = this.currentAudio.file;
     }
   }
