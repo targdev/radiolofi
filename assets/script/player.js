9475e0963572918314013af45d9e5e9fa31fe93f
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-27 11:23:27 -0300

    Update player.js

--- assets/script/player.js
+++ assets/script/player.js
@@ -1,17 +1,40 @@
-window.player = {
-    main: document.querySelector('#main'),
-    title: document.querySelector('.card-content h5'),
-    artist: document.querySelector('.artist'),
-    cover: document.querySelector('.card-image'),
-    audio: document.querySelector('audio'),
+import audios from "./data_base.js"
+import { path } from "./utilites.js"
+import elements from "./player_elements.js"
 
+export default {
     audioData: audios,
     currentAudio: {},
     currentPlaying: 0,
+    isPlaying: false,
   
     start() {
+      elements.set.call(this);
+      elements.actions.call(this)
+
       this.update();
       this.audio.onended = () => this.next();
+    },
+
+    play() {
+      this.isPlaying = true;
+      this.audio.play();
+      this.playPause.innerText = 'pause';
+    },
+
+    pause() {
+      this.isPlaying = false;
+      this.audio.pause();
+      this.playPause.innerText = 'play_arrow';
+    },
+
+    togglePlayPause() {
+      this.isPlaying ? this.pause() : this.play();
+    },
+
+    toggleMute() {
+      this.audio.muted = !this.audio.muted;
+      this.mute.innerText = this.audio.muted ? "volume_off" : "volume_up"
     },
 
     next() {
@@ -27,7 +50,7 @@ window.player = {
       this.title.innerText = this.currentAudio.title;
       this.artist.innerText = this.currentAudio.artist;
       this.cover.style.background = `url(${path(this.currentAudio.cover)}) no-repeat center center / cover`;
-      this.audio.src = path(this.currentAudio.file);
+      elements.createAudioElement.call(this, path(this.currentAudio.file))
     },
 
     restart() {
