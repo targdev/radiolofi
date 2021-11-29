6e8eaad80d1c0276c9c9e0627b1a0e7e32b71a24
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2021-11-29 12:08:32 -0300

    Update player.js

--- assets/script/player.js
+++ assets/script/player.js
@@ -10,7 +10,6 @@ export default {
   
     start() {
       elements.set.call(this);
-      elements.actions.call(this)
 
       this.update();
       this.audio.onended = () => this.next();
@@ -32,6 +31,14 @@ export default {
       this.isPlaying ? this.pause() : this.play();
     },
 
+    setSeek(value) {
+      this.audio.currentTime = value;
+    },
+
+    setVolume(value) {
+      this.audio.volume = value / 100
+    },
+
     toggleMute() {
       this.audio.muted = !this.audio.muted;
       this.mute.innerText = this.audio.muted ? "volume_off" : "volume_up"
@@ -51,6 +58,11 @@ export default {
       this.artist.innerText = this.currentAudio.artist;
       this.cover.style.background = `url(${path(this.currentAudio.cover)}) no-repeat center center / cover`;
       elements.createAudioElement.call(this, path(this.currentAudio.file))
+      this.seekBar.max = this.audio.duration;
+      
+      this.audio.onloadeddata = () => {
+        elements.actions.call(this)
+      }
     },
 
     restart() {
